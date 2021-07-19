6216416cd126b0a9e09849c18031172253554169
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-19 19:47:31 +0530

    created object of Button class and initialized it

--- app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/MainActivity.java
@@ -1,14 +1,16 @@
 package sanskar.kumar.kumarsanskar.movetonextscreenapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button moveToNextScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moveToNextScreen = (Button) findViewById(R.id.button);
     }
 }
