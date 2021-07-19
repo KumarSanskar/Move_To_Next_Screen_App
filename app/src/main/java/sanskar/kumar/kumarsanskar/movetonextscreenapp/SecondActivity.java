0967e2608b13bead8567f6a882ea584856b92bc9
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-19 19:54:25 +0530

    declared,initialized and used Intent to go from this to another screen

--- app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/SecondActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/SecondActivity.java
@@ -1,5 +1,6 @@
 package sanskar.kumar.kumarsanskar.movetonextscreenapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,9 @@ public class SecondActivity extends AppCompatActivity {
         moveToFirstScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent2 = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent2);
+                finish();
             }
         });
     }
