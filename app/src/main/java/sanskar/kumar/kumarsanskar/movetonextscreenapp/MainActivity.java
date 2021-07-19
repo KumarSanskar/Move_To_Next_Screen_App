436f432a82632bc44fab73bfcad5d6fe651518f3
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-19 19:50:12 +0530

    used Intent, declared, initialized and used it

--- app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/MainActivity.java
@@ -1,5 +1,6 @@
 package sanskar.kumar.kumarsanskar.movetonextscreenapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
         moveToNextScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent1 = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent1);
+                finish();
             }
         });
     }
