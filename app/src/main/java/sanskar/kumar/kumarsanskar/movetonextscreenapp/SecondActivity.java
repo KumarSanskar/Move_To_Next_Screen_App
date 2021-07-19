2182d1f3cdfcf525d03a283627bf6069300f938f
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-19 19:52:59 +0530

    used setOnClickListener() with moveToFirstScreen button

--- app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/SecondActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/SecondActivity.java
@@ -1,6 +1,7 @@
 package sanskar.kumar.kumarsanskar.movetonextscreenapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +14,11 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         moveToFirstScreen = (Button) findViewById(R.id.button2);
+        moveToFirstScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
