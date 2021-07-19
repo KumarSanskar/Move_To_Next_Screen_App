e80c033ff1856f44af4a99a4dab323909b4644aa
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-19 19:52:06 +0530

    created and initialized object for second button

--- app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/SecondActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/movetonextscreenapp/SecondActivity.java
@@ -1,14 +1,17 @@
 package sanskar.kumar.kumarsanskar.movetonextscreenapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    Button moveToFirstScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        moveToFirstScreen = (Button) findViewById(R.id.button2);
     }
 }
